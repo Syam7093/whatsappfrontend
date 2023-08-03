52f4411953ba1f897f0fe633fc2eb9bd61f4c6e0
syamsundarsai <msyamsundarsai@gmail.com>
2023-08-03 16:59:49 +0530

    adding

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { ChatIcon } from "./svg";
+
+
+function App() {
+  return (
+    <div>
+      <h1>welcome to the project...</h1>
+      <ChatIcon className="red"></ChatIcon>
+    </div>
+  );
+}
+
+export default App;
